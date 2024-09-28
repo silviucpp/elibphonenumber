f87b19723232c0924bd66a1ff9a28ba7d9cb6174
silviu caragea <silviu.cpp@gmail.com>
2024-09-28 09:00:44 +0300

    Add support for faster builds with ccache

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-elibphonenumber
-===============
-[![Build Status](https://app.travis-ci.com/silviucpp/elibphonenumber.svg?branch=master)](https://travis-ci.com//github/silviucpp/elibphonenumber)
+# elibphonenumber
+
+[![Build Status](https://app.travis-ci.com/silviucpp/elibphonenumber.svg?branch=master)](https://travis-ci.com/github/silviucpp/elibphonenumber)
 [![GitHub](https://img.shields.io/github/license/silviucpp/elibphonenumber)](https://github.com/silviucpp/elibphonenumber/blob/master/LICENSE)
 [![Hex.pm](https://img.shields.io/hexpm/v/elibphonenumber)](https://hex.pm/packages/elibphonenumber)
 
@@ -8,55 +8,65 @@ Erlang driver for [libphonenumber][1].
 
 ## Notes
 
-- This project is active. I'm doing updates very often because I see the original author is no longer maintaining it.
-- Compatible with both `rebar` and `rebar3` or `hex`.
-- To change the `libphonenumber` version modify in `rebar.config` the `DRIVER_REV` argument sent to `make`.
-- By default, the build is using git tag archive rather than full clone and tag checkout. You can change this behaviour by modifying in `rebar.config` the `DRIVER_SRC` argument sent to `git`. 
+- This project is actively maintained, with frequent updates due to the original author's inactivity.
+- Compatible with both `rebar` and `rebar3`, as well as `hex`.
+- To change the `libphonenumber` version, modify the `DRIVER_REV` argument in `rebar.config`.
+- By default, the build uses a git tag archive rather than a full clone and tag checkout. You can change this behavior by modifying the `DRIVER_SRC` argument in `rebar.config`.
 
-## Compile
+## Compilation
 
-In order to compile you need to make sure all dependencies needed to build `libphonenumber` are already installed.
+Ensure all dependencies needed to build `libphonenumber` are installed. Below is a summary for each operating system where the library has been tested. If you encounter issues, consult the [libphonenumber documentation][2] for additional guidance.
 
-Next you can find a resume for each operating system where library was tested but in case you encounter problems you can
-consult as well the documentation from building `libphonenumber` located [here][2]
+### Ubuntu or Debian
 
-##### Ubuntu or Debian
-
-On the latest versions it's enough to do:
+For the latest versions, run:
 
 ```bash
 sudo apt-get install cmake cmake-curses-gui libicu-dev 
 sudo apt-get install libprotobuf-dev protobuf-compiler
 ```
 
-##### CentOS 7
+### CentOS 7
 
-Enable [EPEL][3] (for RE2):
+Enable [EPEL][3] (required for RE2):
+
 ```bash
 sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 ```
 
-And install packages:
+Then install the required packages:
+
 ```bash
 sudo yum install cmake git libicu-devel protobuf-devel protobuf-compiler
 ```
 
-##### Mac Os
+### macOS
 
-On `Mac OS` make sure you have `brew` installed and rebar will automatically install all necessary dependencies.
+Make sure you have `brew` installed. Rebar will automatically install all necessary dependencies.
 
-## Get carrier for number
+### Speeding Up the Compilation Process
 
-In order to do this make sure the application is started then use `phonenumber_to_carrier:carrier_for_number/2` method
+You can use `ccache` to speed up the compilation:
+
+- Install `ccache` on your platform (e.g., `sudo apt-get install ccache` for Debian-based systems or `brew install ccache` for macOS).
+- In `rebar.config.script`, add:
+
+```erlang
+os:putenv("ELIBPHONENUMBER_USE_CCACHE", "1"),
+```
+
+## Get Carrier for a Number
+
+Ensure the application is started, then use the `phonenumber_to_carrier:carrier_for_number/2` method:
 
 ```erlang 
 application:ensure_all_started(elibphonenumber).
 phonenumber_to_carrier:carrier_for_number(<<"44743655551">>, <<"en">>).
 ```
 
-## Get Timezones for a number
+## Get Timezones for a Number
 
-In order to get the timezones associated with a number you can call `phonenumber_to_timezones:timezones_for_number/1` method as follow:
+To retrieve the timezones associated with a number, call `phonenumber_to_timezones:timezones_for_number/1` as follows:
 
 ```erlang 
 application:ensure_all_started(elibphonenumber).
@@ -66,7 +76,7 @@ phonenumber_to_timezones:timezones_for_number(<<"16502530000">>).
 
 ## Geocoding
 
-Using the following method you can get the geographical area of a phone number (in case this information is available).
+To get the geographical area of a phone number (if available), use the following method:
 
 ```erlang 
 application:ensure_all_started(elibphonenumber).
@@ -74,7 +84,7 @@ phonenumber_geocoding:get_geocoding_for_number(<<"+16502530000">>),
 <<"Mountain View, CA">>
 ```
 
-## Run the tests
+## Run the Tests
 
 ```bash
 rebar3 eunit
--- build_deps.sh
+++ build_deps.sh
@@ -3,8 +3,8 @@
 DEPS_LOCATION=_build/deps
 DESTINATION=libphonenumber
 
-if [ -f "$DEPS_LOCATION/$DESTINATION/cpp/build/libphonenumber.a" ]; then
-    echo "libphonenumber fork already exist. delete $DEPS_LOCATION/$DESTINATION for a fresh checkout."
+if [ -f "${DEPS_LOCATION}/${DESTINATION}/cpp/build/libphonenumber.a" ]; then
+    echo "libphonenumber fork already exists. Delete ${DEPS_LOCATION}/${DESTINATION} for a fresh checkout."
     exit 0
 fi
 
@@ -14,8 +14,8 @@ LIB_PHONE_NUMBER_REV=$1
 
 DRIVER_SRC="git"
 if [ "$2" = "archive" ]; then
-  DRIVER_SRC="archive";
-  ARCHIVE_VER=$(echo "$LIB_PHONE_NUMBER_REV" | sed -e 's/^v//g');
+    DRIVER_SRC="archive"
+    ARCHIVE_VER=$(echo "$LIB_PHONE_NUMBER_REV" | sed -e 's/^v//g')
 fi
 
 OS=$(uname -s)
@@ -24,134 +24,154 @@ KERNEL=$(echo $(lsb_release -ds 2>/dev/null || cat /etc/system-release 2>/dev/nu
 echo "Use repo ${LIB_PHONE_NUMBER_REPO} and revision ${LIB_PHONE_NUMBER_REV}"
 echo "OS detected: ${OS} ${KERNEL}"
 
-function fail_check
-{
+function fail_check {
     "$@"
     local status=$?
     if [ $status -ne 0 ]; then
-        echo "error with $1" >&2
+        echo "Error with $1" >&2
         exit 1
     fi
 }
 
-qmake_unix()
-{
-	fail_check cmake \
-        -DCMAKE_C_FLAGS="-fPIC" \
-        -DCMAKE_CXX_FLAGS="-fPIC -std=c++11" \
-        -DCMAKE_INSTALL_PREFIX:PATH=install \
-        -DBUILD_TESTING=OFF \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DUSE_BOOST=OFF \
-        -USE_STDMUTEX=ON \
-        ..
+# Enable ccache based on $ELIBPHONENUMBER_USE_CCACHE environment variable
+
+CCACHE_ENABLED=0
+if [ "$ELIBPHONENUMBER_USE_CCACHE" = "1" ]; then
+    if command -v ccache &> /dev/null; then
+        echo "ccache is enabled and will be used."
+        export PATH="/usr/local/bin/ccache:$PATH"
+        CCACHE_ENABLED=1
+    else
+        echo "ccache is not installed. Proceeding without ccache..."
+    fi
+fi
+
+qmake_unix() {
+    local cmake_args=(
+        -DCMAKE_C_FLAGS="-fPIC"
+        -DCMAKE_CXX_FLAGS="-fPIC -std=c++11"
+        -DCMAKE_INSTALL_PREFIX:PATH=install
+        -DBUILD_TESTING=OFF
+        -DBUILD_SHARED_LIBS=OFF
+        -DUSE_BOOST=OFF
+        -DUSE_STDMUTEX=ON
+    )
+
+    # Add ccache launcher if enabled
+    if [ $CCACHE_ENABLED -eq 1 ]; then
+        cmake_args+=(-DCMAKE_C_COMPILER_LAUNCHER=ccache)
+    fi
+
+    fail_check cmake "${cmake_args[@]}" ..
 }
 
-qmake_darwin()
-{
-    ICU4_DIR=$(brew --prefix icu4c)
-    PROTOBUF_DIR=$(brew --prefix protobuf@3)
+qmake_darwin() {
+    local ICU4_DIR=$(brew --prefix icu4c)
+    local PROTOBUF_DIR=$(brew --prefix protobuf@3)
 
     export PKG_CONFIG_PATH="$ICU4_DIR/lib/pkgconfig"
 
-	fail_check cmake \
-      -DCMAKE_CXX_FLAGS="-std=c++11 -DNDEBUG" \
-      -DCMAKE_INSTALL_PREFIX:PATH=install \
-      -DBUILD_TESTING=OFF \
-      -DBUILD_SHARED_LIBS=OFF \
-      -DUSE_BOOST=OFF \
-      -USE_STDMUTEX=ON \
-      -DICU_UC_INCLUDE_DIR=$ICU4_DIR/include \
-      -DICU_UC_LIB=$ICU4_DIR/lib/libicuuc.dylib \
-      -DICU_I18N_INCLUDE_DIR=$ICU4_DIR/include \
-      -DICU_I18N_LIB=$ICU4_DIR/lib/libicui18n.dylib \
-      -DPROTOBUF_INCLUDE_DIR=$PROTOBUF_DIR/include \
-      -DPROTOBUF_LIB=$PROTOBUF_DIR/lib \
-      -DPROTOC_BIN=$PROTOBUF_DIR/bin/protoc \
-      ..
+    local cmake_args=(
+        -DCMAKE_CXX_FLAGS="-std=c++11 -DNDEBUG"
+        -DCMAKE_INSTALL_PREFIX:PATH=install
+        -DBUILD_TESTING=OFF
+        -DBUILD_SHARED_LIBS=OFF
+        -DUSE_BOOST=OFF
+        -DUSE_STDMUTEX=ON
+        -DICU_UC_INCLUDE_DIR="$ICU4_DIR/include"
+        -DICU_UC_LIB="$ICU4_DIR/lib/libicuuc.dylib"
+        -DICU_I18N_INCLUDE_DIR="$ICU4_DIR/include"
+        -DICU_I18N_LIB="$ICU4_DIR/lib/libicui18n.dylib"
+        -DPROTOBUF_INCLUDE_DIR="$PROTOBUF_DIR/include"
+        -DPROTOBUF_LIB="$PROTOBUF_DIR/lib"
+        -DPROTOC_BIN="$PROTOBUF_DIR/bin/protoc"
+    )
+
+    # Add ccache launcher if enabled
+    if [ $CCACHE_ENABLED -eq 1 ]; then
+        cmake_args+=(-DCMAKE_C_COMPILER_LAUNCHER=ccache)
+    fi
+
+    fail_check cmake "${cmake_args[@]}" ..
 }
 
-install_libphonenumber()
-{
-  if [ "${DRIVER_SRC}" = "git" ]; then
-		echo "Building from Git tag"
-		git clone ${LIB_PHONE_NUMBER_REPO} ${DESTINATION}
-		pushd ${DESTINATION}
-		fail_check git checkout ${LIB_PHONE_NUMBER_REV}
-		popd
-	elif [ "${DRIVER_SRC}" = "archive" ]; then
-		echo "Building from Git archive"
-		fail_check wget -q -O libphonenumber.tar.gz $LIB_PHONE_NUMBER_ARCHIVE
-		fail_check tar -xf libphonenumber.tar.gz
-		fail_check rm libphonenumber.tar.gz
-		fail_check mv libphonenumber-${ARCHIVE_VER} ${DESTINATION}
-	fi;
+install_libphonenumber() {
+    if [ "${DRIVER_SRC}" = "git" ]; then
+        echo "Building from Git tag"
+        fail_check git clone "${LIB_PHONE_NUMBER_REPO}" "${DESTINATION}"
+        pushd "${DESTINATION}" || exit
+        fail_check git checkout "${LIB_PHONE_NUMBER_REV}"
+        popd || exit
+    elif [ "${DRIVER_SRC}" = "archive" ]; then
+        echo "Building from Git archive"
+        fail_check wget -q -O libphonenumber.tar.gz "$LIB_PHONE_NUMBER_ARCHIVE"
+        fail_check tar -xf libphonenumber.tar.gz
+        fail_check rm libphonenumber.tar.gz
+        fail_check mv "libphonenumber-${ARCHIVE_VER}" "${DESTINATION}"
+    fi;
 
-	mkdir -p ${DESTINATION}/cpp/build
-	pushd ${DESTINATION}/cpp/build
+    mkdir -p "${DESTINATION}/cpp/build"
+    pushd "${DESTINATION}/cpp/build" || exit
 
-	case $OS in
+    case $OS in
         Linux)
             qmake_unix
-        ;;
-
+            ;;
         Darwin)
             qmake_darwin
-        ;;
-
+            ;;
         *)
             echo "Your system $OS is not supported"
             exit 1
     esac
 
-	fail_check make -j 8
-	fail_check make install
-	popd
+    fail_check make -j 8
+    fail_check make install
+    popd || exit
 }
 
-copy_resources()
-{
+copy_resources() {
     rm -rf priv
     fail_check mkdir priv
-    fail_check cp -R $DEPS_LOCATION/$DESTINATION/resources/carrier priv/carrier
-    fail_check cp -R $DEPS_LOCATION/$DESTINATION/resources/timezones priv/timezones
+    fail_check cp -R "${DEPS_LOCATION}/${DESTINATION}/resources/carrier" priv/carrier
+    fail_check cp -R "${DEPS_LOCATION}/${DESTINATION}/resources/timezones" priv/timezones
 }
 
-run_installation()
-{
-	mkdir -p $DEPS_LOCATION
-	pushd $DEPS_LOCATION
+run_installation() {
+    mkdir -p "$DEPS_LOCATION"
+    pushd "$DEPS_LOCATION" || exit
 
     case $OS in
-      Linux)
-         case $KERNEL in
-            Ubuntu|Debian)
-                echo "Check Dependecies for $KERNEL"
-                fail_check dpkg -s cmake cmake-curses-gui libicu-dev protobuf-compiler libprotobuf-dev
-                install_libphonenumber
-                ;;
-            CentOS|Amazon)
-                echo "Check Dependecies for $KERNEL"
-                fail_check rpm -q --dump cmake libicu-devel protobuf-compiler protobuf-devel
-                install_libphonenumber
-                ;;
-            *)
-                echo "Your system $KERNEL is not supported"
-         esac
+        Linux)
+            case $KERNEL in
+                Ubuntu | Debian)
+                    echo "Check Dependencies for $KERNEL"
+                    fail_check dpkg -s cmake cmake-curses-gui libicu-dev protobuf-compiler libprotobuf-dev
+                    install_libphonenumber
+                    ;;
+                CentOS | Amazon)
+                    echo "Check Dependencies for $KERNEL"
+                    fail_check rpm -q --dump cmake libicu-devel protobuf-compiler protobuf-devel
+                    install_libphonenumber
+                    ;;
+                *)
+                    echo "Your system $KERNEL is not supported"
+                    exit 1
+            esac
             ;;
-      Darwin)
+        Darwin)
             export HOMEBREW_NO_INSTALL_UPGRADE=true
             export HOMEBREW_NO_INSTALL_CLEANUP=true
             export HOMEBREW_NO_AUTO_UPDATE=1
             brew install cmake pkg-config icu4c protobuf@3 wget
             install_libphonenumber
             ;;
-      *)
+        *)
             echo "Your system $OS is not supported"
             exit 1
     esac
 
-    popd
+    popd || exit
 }
 
 run_installation
